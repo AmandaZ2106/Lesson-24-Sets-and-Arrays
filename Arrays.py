04a7e5ef216d7c2ff6d1c34329f16e7fd575fa26
AmandaZ2106 <romeogcruz@outlook.com>
2025-10-29 17:56:22 +0000

    Arrays

---
+++ Arrays.py
@@ -0,0 +1,6 @@
+import array as arr
+my_array=arr.array("i",[12,32,12,44,12,23])
+print("Array:",my_array)
+print("Occurence of 12 in the array:",my_array.count(12))
+my_array.reverse()
+print("Reversed Array:",my_array)
