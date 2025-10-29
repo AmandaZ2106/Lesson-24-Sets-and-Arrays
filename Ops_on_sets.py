edee55eda1fbeb491d85369dbd8d02c074f54054
AmandaZ2106 <romeogcruz@outlook.com>
2025-10-29 17:45:49 +0000

    Operations on sets

---
+++ Ops_on_sets.py
@@ -0,0 +1,12 @@
+my_set={3,4,7,1}
+print(my_set)
+my_set={54,"Hello",True}
+print(my_set)
+my_set={1,2,3,4,1,2,3,4}
+print(my_set)
+my_set=set([12,23,24,12,12,14])
+print(my_set)
+my_set={1,3,45,23,35}
+print("Original set:",my_set)
+my_set.pop()
+print("Set after deleting an element:",my_set)
