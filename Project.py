73085b062815be8234e1f4d6ee31a753b52d2f02
AmandaZ2106 <romeogcruz@outlook.com>
2025-10-29 18:20:03 +0000

    Project

--- Project.py
+++ Project.py
@@ -1,5 +1,12 @@
-set1={1,2,3,4,5,6,7}
-set2={1,3,5,7}
+set1={"blue","green"}
+set2={"blue","yellow"}
+print("Set 1:",set1)
+print("Set 2:",set2)
+set3=set1.difference(set2)
+print("The numbers that are not common in both sets are:",set3)
+
+set1={1,2,3,4,5}
+set2={1,5,6,7,8,9}
 print("Set 1:",set1)
 print("Set 2:",set2)
 set3=set1.difference(set2)
